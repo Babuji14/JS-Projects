1dca708aa4855f80db6a0620262597e3cea92012
Babu Mani <156921718+Babuji14@users.noreply.github.com>
2024-09-29 08:48:38 +0530

    add copy functionalities

--- Color Code Generator/Script.js
+++ Color Code Generator/Script.js
@@ -1,9 +1,12 @@
 const codeContainer=document.querySelector(".color-container");
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 20; i++) {
     const codeContainerEl=document.createElement("div");
+    const icon=document.createElement("i");
+    icon.classList.add("fa-regular", "fa-copy");
     codeContainerEl.classList.add("code-container");
-    codeContainer.appendChild(codeContainerEl)
+    codeContainer.appendChild(codeContainerEl);
+    codeContainer.appendChild(icon);
 }
 
 const codel=document.querySelectorAll(".code-container");
@@ -14,7 +17,19 @@ function generator() {
         const newcolorcode = randomColor();
         codeelement.style.backgroundColor = "#" + newcolorcode;
         codeelement.innerHTML = `<span class="color-code">#${newcolorcode}</span> <i class="fa-regular fa-copy"></i>`;
+    
+    const copyicon=codeelement.querySelector(".fa-copy");
+    copyicon.addEventListener("click", ()=>{
+        copyToClipboard(`#${newcolorcode}`);
+        
+        codeelement.innerHTML = `<span class="color-code">Copied!</span> <i class="fa-regular fa-copy"></i>`;
+        
+        setTimeout(() => {
+            codeelement.innerHTML = `<span class="color-code">#${newcolorcode}</span> <i class="fa-regular fa-copy"></i>`;
+        }, 1500);
+        });
     });
+
 }
 
 // Function to generate a random 6-digit hex color code
@@ -27,6 +42,16 @@ function randomColor(){
         colorCode+=characters.substring(randomNum, randomNum+1);
     }
     return colorCode;
+}
+
+// Function to copy text to clipboard
+function copyToClipboard(text) {
+    const tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = text;
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
 }
 
 generator();
