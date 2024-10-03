ae3739c564f57319b738d1598b0aec3a7e7f22a8
Babu Mani <156921718+Babuji14@users.noreply.github.com>
2024-10-03 20:14:08 +0530

    problem with download an image

--- Random Images/Script.js
+++ Random Images/Script.js
@@ -4,7 +4,11 @@ const pictures = document.querySelectorAll(".pictures")
 const modelContainer = document.querySelector(".model-container");
 const modelImg = document.querySelector(".model-img");
 const closeBtn = document.querySelector(".close");
-let index=0;
+const preBtn=document.querySelector(".btn-prev");
+const nxtBtn=document.querySelector(".btn-next");
+let currentIndex = 0;
+const downloadButton=document.querySelector(".download-btn");
+
 
 btn.addEventListener("click", () => {
     images = 2;
@@ -29,6 +33,7 @@ function addNewImages() {
 
 pictures.forEach((pics, i) => {
     pics.addEventListener("click", function() {
+        currentIndex=i;
         modelContainer.classList.add("active");
         modelImg.src = pics.src;
     });
@@ -38,3 +43,21 @@ closeBtn.addEventListener("click", function () {
     modelContainer.classList.remove("active");
 });
 
+nxtBtn.addEventListener("click", function(){
+    currentIndex=(currentIndex+1)%pictures.length;
+    modelImg.src=pictures[currentIndex].src;
+});
+
+preBtn.addEventListener("click", function(){
+    currentIndex=(currentIndex-1 +pictures.length)%pictures.length;
+    modelImg.src=pictures[currentIndex].src;
+});
+
+function src(imageLink){
+    modelImg.src=imageLink;
+    downloadImgsrc(imageLink);
+}
+
+function downloadImgsrc(imageSrc){
+    downloadButton.href=imageSrc;
+}
